7483c658638df13e8d179e5ae12cd774d1b20788
Razvan <brb_end1@yahoo.com>
2020-06-24 05:40:11 +0200

    solve repeat string function

--- scripts/handlers/repeat-string.js
+++ scripts/handlers/repeat-string.js
@@ -7,22 +7,21 @@ function repeatStringHandler() {
   debugger;
   console.log('\n--- action: repeat string ---');
   console.log('range:', typeof range, '\n', range);
-  console.log('stringToRepeat:', typeof stringToRepeat, '\n', stringToRepeat);
 
   // read inputs from user
   const stringToRepeat = prompt('enter a string to repeat');
   console.log('stringToRepeat:', typeof stringToRepeat, '\n', stringToRepeat);
-  if (stringToRepeat !== null) {
+  if (typeof stringToRepeat !== 'string') {
     return;
   }
 
   // perform core logic
-  let repeatedString;
+  let repeatedString ='';
   for (let i = 1; i < range; i++) {
-    repeatedString += repeatedString;
+    repeatedString += `${stringToRepeat}`;
   }
   console.log('repeatedString:', typeof repeatedString, '\n', repeatedString);
 
   // alert result for the user
-  alert(stringToRepeat);
+  alert(repeatedString);
 }
